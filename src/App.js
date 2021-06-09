f9fd6662fbcbf7fa8190a939724d42220e7cc85b
multani princess <nazs09364@gmail.com>
2021-06-09 01:10:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <table className="titleBar">
+       <tbody>
+         <tr>
+           
+          </tr>
+          
+           
+          
+        </tbody>
+      </table> 
+      <input style={{
+         fontSize: 15,
+         panddingTop:8,
+         panddingBorder:8,
+         paddingLeft:16,
+         display:'black',
+      }} 
+      placeholder="Enter search term" /> 
+      <h2 >All movies</h2>  
     </div>
   );
-}
+    }
 
 export default App;
